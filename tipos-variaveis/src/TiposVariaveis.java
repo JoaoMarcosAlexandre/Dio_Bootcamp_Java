121d7ef552bb94180b38fe9a07f308df263bcf62
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-02 18:44:41 -0300

    Tipos de variáves para estudos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
+    @SuppressWarnings("unused")
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 29;
+        short ano = 2024;
+        int cep = 17600650; //se começar com zero, talvez deva utilizar outro tipo
+        long cpf = 12345678900L; //se começar com zero, pode ser que seja necessário outro tipo
+        float pi = 3.14F; //tem que colocar F no fim sempre pra evitar erro
+        double salarioMinimo = 1345.53; 
+        //tipo long deve ser encerrado com L para representar o numero longo
+        
+        final double VALOR_PI = 3.1418F;
+            System.out.println(VALOR_PI);
     }
 }
