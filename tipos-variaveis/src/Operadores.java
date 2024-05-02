7bb5d81a53a12e9a45e865207b0854a93e63def2
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-02 19:44:07 -0300

    Aula sobre operadores, aritiméticos e booleanos

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,68 @@
+public class Operadores {
+    public static void main(String[] args) {
+    // String nomeCompleto = "Linguagem " + "JAVA"; //sinal de + concatena strings
+    //   System.out.println(nomeCompleto);
+
+   // int numero = 15;
+   // numero = - numero; ///para numeros negativos, multiplicar por -1 para tornar positivo
+   //    System.out.println(numero);
+
+        //incremento de numeros
+
+       // int numero = 5;
+         //   System.out.println(++ numero);
+           // System.out.println(numero);
+
+
+   // boolean variavel = true;
+   // variavel = !variavel;
+   //  System.out.println(variavel);
+
+   ///TERNARIOS - comparáveis com if, representado por : e ?
+
+    /*   int a, b;
+
+        a = 5;
+        b = 6;
+
+        String resultado = a == b ?"verdadeiro" : "falso"; //forma simplificada da estrutura if else
+            
+        System.out.println(resultado);
+*/
+
+        /* COMPARADORES
+         * == verifica igualdade
+         * != diferença
+         * >= maior ou igual
+         * > maior que
+         * <=menor ou igual
+         * < menor que
+         **************************
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 <= numero2;
+            System.out.println("Numero 1 é menor ou igual que numero 2? " + simNao);
+*/
+/*          Comparando textos
+            String nomeUm = "João";
+          String nomeDois = new String("João");
+            System.out.println(nomeUm.equals(nomeDois)); */
+       
+       
+       
+            /*Operadores lógicos
+         * && operador E
+         * || operador OU
+         
+
+         boolean condicao1 = true;
+         boolean condicao2 = false;
+            if (condicao1 || condicao2){
+                System.out.println("uma das condições é verdadeira");
+            }
+            System.out.println("FIM");*/
+    }
+
+}
