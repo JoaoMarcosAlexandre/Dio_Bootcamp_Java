5e273a67a38e29015ed87a0e2d32dd76722c54aa
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-06 18:35:20 -0300

    feat: aula de métodos

---
+++ tipos-variaveis/src/Metodos.java
@@ -0,0 +1,20 @@
+public class Metodos {
+
+  /*  public double somar (int num1, int num2){
+        return ...;
+    }
+
+    public void imprimir (String texto){
+        //aqui não precisa de retorno, sabendo que é void
+    }*/
+
+   /* public double dividir (int dividendo, int divisor) throws Exception{
+        return divisor;
+        *deve haver a exceção devido não poder dividir por 0
+        */
+        
+      /*public void validar (){
+        //deveria retornar um valor booleano
+      }  */
+
+}
