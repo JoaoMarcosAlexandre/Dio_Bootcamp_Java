379193aeb9a909dae8ce35c45901b8960c0b26bb
JoaoAlexandre <alexandremjoao@gmail.com>
2024-05-07 13:48:48 -0300

    feat: "add Scanner object"

--- terminal-argumentos/src/AboutMe.java
+++ terminal-argumentos/src/AboutMe.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+
 public class AboutMe {
-    public static void main(String[] args){
+
+    //Criando um objeto Scanner
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome: ");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade: ");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura: ");
+        double altura = scanner.nextDouble();
+
+
+        System.out.println("Olá, meu nome é " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos");
+        System.out.println("Minha altura é " + altura + " cm.");
+
+    }
+
+   /* public static void main(String[] args){
         //Os argumentos começam com indice 0
         String nome = args [0];
         String sobreNome = args [1];
@@ -8,7 +36,7 @@ public class AboutMe {
         
     System.out.println("Olá, meu nome é " + nome + " " + sobreNome);
     System.out.println("Tenho " + idade + " anos");
-    System.out.println("Minha altura é " + altura + " cm.");
+    System.out.println("Minha altura é " + altura + " cm.");*/
 
         /*Abrindo no PowerShell
          * 
@@ -16,5 +44,4 @@ public class AboutMe {
          * 
          */
 
-    }
 }
